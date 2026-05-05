c9993f0ef5f419d6f88f2fe6c4960b0ea2a04a92
dax <dax26d@gmail.com>
2026-05-05 14:58:21 +0800

    feat(core): 引入UI文件分离主窗口界面

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # A pyside6 learning project
 
 from PySide6.QtWidgets import QApplication, QMainWindow
+from forms.mainwindow_ui import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My First PySide6 App")
-        self.setGeometry(100, 100, 800, 600)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.show()
 
 if __name__ == "__main__":
